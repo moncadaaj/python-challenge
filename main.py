9b1d0407e467d465a6c60ab3b79afa9c0f0bc9d8
moncadaaj <moncadaaj122@gmail.com>
2024-06-01 13:16:13 -0700

    Add main.py, PyBank/, and PyPoll/

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import csv
+
+csv_path = os.path.join("Resources", "data.csv")
+
+def main():
+    # Add your analysis code here
+    print(f"Reading data from {csv_path}")
+
+if __name__ == "__main__":
+    main()
